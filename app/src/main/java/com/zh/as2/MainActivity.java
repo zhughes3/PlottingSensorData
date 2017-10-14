f6e53274b7637183a6d536cfa3d66fb517fc892e
zhughes3 <zhughes3@gmail.com>
2017-10-14 16:20:15 -0400

    Accelerometer button added with details in TextView

--- app/src/main/java/com/zh/as2/MainActivity.java
+++ app/src/main/java/com/zh/as2/MainActivity.java
@@ -1,13 +1,53 @@
 package com.zh.as2;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+    private SensorManager sm;
+    private Sensor temp;
+    private List<Sensor> l;
+    private Sensor accel;
+    private TextView accelText;
+    //private LinearLayout tempLayout = (LinearLayout) findViewById(R.id.tempLayout);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout);
+        sm = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        l = sm.getSensorList(Sensor.TYPE_ALL);
+        accel = sm.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        accelText = (TextView) findViewById(R.id.accelText);
+        sm.registerListener(this, accel, SensorManager.SENSOR_DELAY_NORMAL);
+        if (accel != null) {
+            accelText.setText(getSensorInfo(accel));
+        }
+        //temp = sm.getDefaultSensor(Sensor.TYPE_AMBIENT_TEMPERATURE);
+
+    }
+
+    private String getSensorInfo(Sensor s) {
+        return s.getName() + "\n" + s.getMaximumRange() + "\n" + s.getResolution() + "\n"
+                + s.getMinDelay();
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
     }
 }
