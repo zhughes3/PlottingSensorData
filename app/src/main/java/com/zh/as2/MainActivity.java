135c3f899c26bcfa53be52ed419c829fbbc1b7a4
zhughes3 <zhughes3@gmail.com>
2017-10-16 18:19:08 -0400

    PlotView working for Accelerometer

--- app/src/main/java/com/zh/as2/MainActivity.java
+++ app/src/main/java/com/zh/as2/MainActivity.java
@@ -1,48 +1,69 @@
 package com.zh.as2;
 
 import android.content.Context;
+import android.content.Intent;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.LinearLayout;
+import android.view.View;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
     private SensorManager sm;
-    private Sensor temp;
+    private Sensor mag;
     private List<Sensor> l;
+    private List<AccelerometerReading> readings;
     private Sensor accel;
     private TextView accelText;
-    //private LinearLayout tempLayout = (LinearLayout) findViewById(R.id.tempLayout);
+    private TextView magText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout);
+        readings = new ArrayList<>();
         sm = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         l = sm.getSensorList(Sensor.TYPE_ALL);
         accel = sm.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mag = sm.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
         accelText = (TextView) findViewById(R.id.accelText);
+        magText = (TextView) findViewById(R.id.magText);
         sm.registerListener(this, accel, SensorManager.SENSOR_DELAY_NORMAL);
+        sm.registerListener(this, mag, SensorManager.SENSOR_DELAY_NORMAL);
         if (accel != null) {
-            accelText.setText(getSensorInfo(accel));
+            accelText.setText(getSensorInfo(accel, "Accelerometer"));
         }
-        //temp = sm.getDefaultSensor(Sensor.TYPE_AMBIENT_TEMPERATURE);
+        if (mag != null) {
+            magText.setText(getSensorInfo(mag, "Magnetic Field"));
+        }
 
     }
 
-    private String getSensorInfo(Sensor s) {
-        return s.getName() + "\n" + s.getMaximumRange() + "\n" + s.getResolution() + "\n"
-                + s.getMinDelay();
+    private String getSensorInfo(Sensor s, String sensorName) {
+        return "Status: " + sensorName + " is Present\n"
+                + "Max Range: " + s.getMaximumRange() + "\n"
+                + "Resolution: " + s.getResolution() + "\n"
+                + "Min Delay: " + s.getMinDelay();
+    }
+
+    public void toAccelPlot(View view) {
+        Intent intent = new Intent(MainActivity.this, AccelerometerActivity.class);
+        startActivity(intent);
+    }
+
+    public void toMagPlot(View view) {
+        Intent intent = new Intent(MainActivity.this, MagActivity.class);
+        startActivity(intent);
     }
 
     @Override
-    public void onSensorChanged(SensorEvent sensorEvent) {
+    public void onSensorChanged(SensorEvent event) {
 
     }
 
--- app/src/main/java/com/zh/as2/PlotView.java
+++ app/src/main/java/com/zh/as2/PlotView.java
@@ -18,16 +18,32 @@ import java.util.List;
 public class PlotView extends View {
     int MAX = 100;
     int LIST_SIZE = 10;
-    List<Float> points = new ArrayList<>(LIST_SIZE);
+    List<Float> values = new ArrayList<>(LIST_SIZE);
+    List<Float> means = new ArrayList<>(LIST_SIZE);
+    List<Float> variances = new ArrayList<>(LIST_SIZE);
 
     public void clearList() {
-        points = new ArrayList<>(LIST_SIZE);
+        values = new ArrayList<>(LIST_SIZE);
     }
 
-    public void addPoint(float f) {
-        points.add(f);
-        if (points.size() > LIST_SIZE)
-            points.remove(0);
+    public void addValue(float f) {
+        values.add(f);
+        if (values.size() > LIST_SIZE)
+            values.remove(0);
+    }
+
+    public void addMean(float f) {
+        means.add(f);
+        if (means.size() > LIST_SIZE) {
+            means.remove(0);
+        }
+    }
+
+    public void addVariance(float f) {
+        variances.add(f);
+        if (variances.size() > LIST_SIZE) {
+            variances.remove(0);
+        }
     }
 
     public PlotView(Context context) {
@@ -50,19 +66,35 @@ public class PlotView extends View {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
-        Paint p = new Paint(Color.BLUE);
-
         int x = this.getWidth(),
                 y = this.getHeight(),
-                size = points.size(),
+                size = values.size(),
                 dx = this.getWidth()/LIST_SIZE,
                 dy = this.getHeight()/MAX;
 
+        drawCartesianPlane(canvas, x, y);
 
         for (int i = 0; i < size; i++) {
-            canvas.drawCircle(i * dx, points.get(i) * dy, 5, p);
+            Paint p = new Paint();
+            p.setColor(Color.RED);
+            canvas.drawCircle(i * dx, values.get(i) * dy, 5, p);
+            p.setColor(Color.BLUE);
+            canvas.drawCircle(i * dx, means.get(i) * dy, 5, p);
+            p.setColor(Color.GREEN);
+            canvas.drawCircle(i * dx, variances.get(i) * dy, 5, p);
         }
 
         //invalidate();
+    }
+
+    private void drawCartesianPlane(Canvas canvas, int x, int y) {
+        Paint axis = new Paint();
+        axis.setColor(Color.GRAY);
+        canvas.drawLine(0,y-10,x,y-10,axis); //draw x axis
+        //label x axis
+        //draw horizontal gridlines
+        canvas.drawLine(0,y,0,0,axis); //draw y axis
+        //label y axis
+        //draw vertical gridlines
     }
 }
